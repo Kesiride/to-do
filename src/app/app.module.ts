3e3afb259ef1ad0151528f6e7f6053e62f0fc1b8
Kesiride Nwokocha <kesiridenwokocha@Kesirides-MBP.attlocal.net>
2019-10-27 12:32:40 -0700

    Added home and add-to-list componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddToListComponent } from './add-to-list/add-to-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddToListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
