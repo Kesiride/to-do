40b7e006187cd5dd8086035a193e7da61efbe99d
Kesiride Nwokocha <kesiridenwokocha@Kesirides-MBP.attlocal.net>
2019-10-27 13:18:09 -0700

    Added Materials Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddToListComponent } from './add-to-list/add-to-list.component';
 import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './materials/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/materials/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { MatCardModule } from "@angular/material";
+
+const MaterialComponents = [
+    MatCardModule
+]
+
+@NgModule({
+    declarations: [],
+    imports: [MaterialComponents],
+    exports: [MaterialComponents]
+})
+
+export class MaterialModule{}
